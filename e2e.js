1ad7aff3c0a4de4c5bcdfbf271e48858c71e3eac
Nir Naor <nirnaori@gmail.com>
2021-07-23 14:08:47 +0300

    adding tests script.

---
+++ e2e.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('e2e', function() {
+    it('should open browser and do something', function() {
+      assert.equal([1, 2, 3, 4].indexOf(4), -1);
+    });
+  });
+});
