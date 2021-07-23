3e20134b9aea5b6bfb5e0ad70cef946977cec548
Nir Naor <nirnaori@gmail.com>
2021-07-23 14:32:22 +0300

    adding deploy.

--- e2e.js
+++ e2e.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('Array', function() {
   describe('e2e', function() {
     it('should open browser and do something', function() {
-      assert.equal([1, 2, 3, 4].indexOf(4), -1);
+      assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
 });
