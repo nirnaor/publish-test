2c78db60c93c32b1d25aaf4474a4d79d8859f1be
Nir Naor <nirnaori@gmail.com>
2021-10-20 13:05:42 +0300

    consuming.

--- packages/p-consumer/index.js
+++ packages/p-consumer/index.js
@@ -1,3 +1,5 @@
+const nirwixotherpackage = require('nirwixotherpackage')
 exports.testConsumption = function() {
   console.log("will test consumption of nirwixotherpackage");
+  nirwixotherpackage.printMsg()
 }
